997b214d6a54713a38cfdd6a85d19b27fa3989cb
kavalar <apuc06@mail.ru>
2018-02-05 17:44:50 +0300

    create desctop

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,4 +3,10 @@ import $ from 'jquery';
 
 $(() => {
 	svg4everybody();
+
+	$(document).on('click', '.settings__title', function () {
+		$(this).siblings('.settings__col2-body').slideToggle('fast');
+		$(this).toggleClass('settings__title--active');
+	});
+
 });
