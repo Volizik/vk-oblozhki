62e138b33eaa86055ec8070f0f37299e128fc33a
kavalar <apuc06@mail.ru>
2018-02-07 17:50:13 +0300

    jquery

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,8 @@
-import svg4everybody from 'svg4everybody';
-import $ from 'jquery';
+// import svg4everybody from 'svg4everybody';
+// import $ from 'jquery';
 
 $(() => {
-	svg4everybody();
+	// svg4everybody();
 
 	$(document).on('click', '.settings__title', function () {
 		$(this).siblings('.settings__col2-body').slideToggle('fast');
